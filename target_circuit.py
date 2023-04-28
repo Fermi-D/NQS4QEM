3943a841d1f3306e2fa5e755c6dd3110865214ee
Fermi-D <ryomaekura@g.ecc.u-tokyo.ac.jp>
2023-04-28 23:01:38 +0900

    add code

--- target_circuit.py
+++ target_circuit.py
@@ -1,24 +1,30 @@
 import numpy as np
-import qulacs
 
 import init_state as ini
 import error_model.depolarizing_error
 import error_model.unitary_error
-import quantum_gate as qg
+import quantum_gate as gate
+
+def Bell(state_name, n_qubit, error_model):
+    if state_name == "state_vector":
+        state_vector = ini.init_state(n_qubit, state_name)
+        state_vector = state_vector @ gate.H(n_qubit, 0)
+        state_vector = state_vector @ gate.Cx(n_qubit, 0, 1)
+    
+    if state_name == "density_matrix":
+        density_matrix = ini.init_state(n_qubit, state_name)
+        density_matrix = gate.H(n_qubit, 0) @ density_matrix @ gate.H(n_qubit, 0)
 
 def GHZ(state_name, n_qubit, error_model):
-    try:
-        if state_name == "state_vector":
-            init_vector = ini.init_state(n_qubit, state_name)
-            state_vector = init_vector @ qg.H(n_qubit, 0)
-            state_vector = state_vector @ qg.Cx(n_qubit, 0, 1)
-            
+    if state_name == "state_vector":
+        state_vector = ini.init_state(n_qubit, state_name)
+        state_vector = state_vector @ gate.H(n_qubit, 0)
+        for i in range(n_qubit-1):
+            state_vector = state_vector @ gate.Cx(n_qubit, 0, i+1)
         
-        if state_name == "density_matrix":
-    
-    except:
-        print("state name is state_vector or density_matrix")
+        return state_vector
         
+    if state_name == "density_matrix":
   
 """
 def random_circuit():
