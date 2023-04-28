e1f82459fc19bdb8b9805cef0eca3557d7fd1426
Fermi-D <ryomaekura@g.ecc.u-tokyo.ac.jp>
2023-04-29 01:32:26 +0900

    add code

---
+++ PyQNE/.ipynb_checkpoints/error_model-checkpoint.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def depolarizing_error(state, error_rate, target_qubit_idx):
+    I = np.eye(2)
+    
+
+def unitary_error(state, theta, target_qubit_idx):
+    #theta = np.sqrt(error_rate)
+    
