a90ffee3fb96603367e12d0d4f62ea35a3b21fc4
Fermi-D <ryomaekura@g.ecc.u-tokyo.ac.jp>
2023-05-09 23:52:35 +0900

    add code

--- PyQNE/target_circuit.py
+++ PyQNE/target_circuit.py
@@ -20,8 +20,8 @@ def Bell(n_qubit, state_name, error_model, error_rate):
     if state_name == "state_vector":
         if error_model == "ideal":
             state_vector = init_state(n_qubit, state_name)
-            state_vector = state_vector @ gate.H(n_qubit,0)
-            state_vector = state_vector @ gate.Cx(n_qubit,0,1)
+            state_vector = gate.H(n_qubit,0) @ state_vector
+            state_vector = gate.Cx(n_qubit,0,1) @ state_vector
         
         return state_vector
     
@@ -70,9 +70,9 @@ def GHZ(n_qubit, state_name, error_model):
     if state_name == "state_vector":
         if error_model == "ideal":
             state_vector = init_state(n_qubit, state_name)
-            state_vector = state_vector @ gate.H(n_qubit, 0)
+            state_vector = gate.H(n_qubit, 0) @ state_vector
             for i in range(n_qubit-1):
-                state_vector = state_vector @ gate.Cx(n_qubit, 0, i+1)
+                state_vector = gate.Cx(n_qubit, 0, i+1) @ state_vector
             
             return state_vector
         
