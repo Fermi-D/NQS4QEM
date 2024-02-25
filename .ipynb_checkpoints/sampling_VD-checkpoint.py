a7cd565c1ed370878082b46aaf94a8c927d08a88
Fermi-D <ryomaekura@g.ecc.u-tokyo.ac.jp>
2024-02-25 23:35:48 +0900

    update

---
+++ .ipynb_checkpoints/sampling_VD-checkpoint.py
@@ -0,0 +1,113 @@
+import torch
+import qucumber.utils.cplx as cplx
+import qucumber.utils.data as data
+from qucumber.observables import ObservableBase, to_pm1
+from qucumber.observables.pauli import flip_spin
+import qucumber
+
+class GeneralPauliDistill(ObservableBase):
+    def __init__(self, pauli_dict: dict, m: int) -> None:
+        self.name = "distilled_pauli"
+        self.symbol = "distilled_general_pauli"
+        self.pauli_dict = pauli_dict
+        self.num_copy = m
+        
+    def apply(self, nn_state, samples):
+        """
+        This function calcualte <x1 x2 ... xm | rho^{\otimes m} O | xm x1 x2 ... xm-1> / <x1 x2 ... xm | rho^{\otimes m} | x1 x2 ... xm>
+        where O acts only on the first register.
+        """
+        
+        # [num_sample, num_visible_node]
+        # samples = [s1, s2, s3 ... sN]
+        #  where num_sample = N, and si is num_visible_node-bits
+        samples = samples.to(device=nn_state.device)
+        
+        num_sample, num_visible_node = samples.shape
+        
+        # [num_sample, num_visible_node * num_copy]
+        # samples_array = [[s1 sN sN-1], [s2 s1 sN], [s3 s2 s1],.. [sN sN-1 sN-2]]
+        #  each row is num_copy*num_visible_node bits the above example is for num_copy=3
+        samples_array = []
+        for copy_index in range(self.num_copy):
+            rolled_samples = torch.roll(samples, shifts=copy_index, dims=0)
+            samples_array.append(rolled_samples)
+        samples_array = torch.hstack(samples_array)
+        assert(samples_array.shape[0] == num_sample)
+        assert(samples_array.shape[1] == num_visible_node * self.num_copy)
+        
+        # roll second dim of [num_sample, num_visible_node * num_copy] by num_visible_node
+        # swapped_samples_array = [[sN-1 s1 sN], [sN s2 s1], [s1 s3 s2],.. [sN-2 sN sN-1]]
+        swapped_samples_array = torch.roll(samples_array, shifts = num_visible_node, dims=1)
+
+        # pick copy of first block
+        #  first_block_sample = [sN-1, sN, s1, s2, ... sN-2]
+        first_block_sample = swapped_samples_array[:, :num_visible_node].clone()
+
+        # calculate coefficient for first block [num_samples, 0:num_visible_node]
+        total_prod = cplx.make_complex(torch.ones_like(samples[:,0]), torch.zeros_like(samples[:,0]))
+        for index, pauli in self.pauli_dict.items():
+            assert(index < num_visible_node)
+            coeff = to_pm1(first_block_sample[:, index])
+            if pauli == "Z":
+                coeff = cplx.make_complex(coeff, torch.zeros_like(coeff))
+                total_prod = cplx.elementwise_mult(coeff, total_prod)
+            elif pauli == "Y":
+                coeff = cplx.make_complex(torch.zeros_like(coeff), coeff)
+                total_prod = cplx.elementwise_mult(coeff, total_prod)
+        
+        # flip samples for for first block [num_samples, 0:num_visible_node]
+        # first_block_sample -> [OsN-1, OsN, Os1, Os2, ... OsN-2]
+        #  where Osi is bit array after Pauli bit-flips 
+        for index, pauli in self.pauli_dict.items():
+            assert(index < num_visible_node)
+            if pauli in ["X", "Y"]:
+                first_block_sample = flip_spin(index, first_block_sample)
+
+
+        # store flipped first block
+        swapped_samples_array[:, :num_visible_node] = first_block_sample
+
+        # calculate product of coefficients
+        # samples_array = [[s1 sN sN-1], [s2 s1 sN], [s3 s2 s1],.. [sN sN-1 sN-2]]
+        # swapped_samples_array = [[OsN-1 s1 sN], [OsN s2 s1], [Os1 s3 s2],.. [OsN-2 sN sN-1]]
+        """
+        total_prod = [
+            <s1 sN sN-1 | rho^{\otimes 3} | OsN-1 s1 sN> / <s1 sN sN-1 | rho^{\otimes 3} | s1 sN sN-1> , 
+            <s2 s1 sN   | rho^{\otimes 3} | OsN s2 s1>   / <s2 s1 sN   | rho^{\otimes 3} | s2 s1 sN> , 
+            <s3 s2 s1   | rho^{\otimes 3} | Os1 s3 s2>   / <s3 s2 s1   | rho^{\otimes 3} | s3 s2 s1> , 
+
+        e.g. 
+        <s3 s2 s1   | rho^{\otimes 3} | Os1 s3 s2>   / <s3 s2 s1   | rho^{\otimes 3} | s3 s2 s1>
+         = <s3 | rho | Os1> <s2 | rho | s3> < s1| rho | s2> / (<s3 | rho | s3> <s2 | rho | s2> < s1| rho | s1>)
+         =  (<s3 | rho | Os1> / <s3 | rho | s3>)
+          * (<s2 | rho | s3> / <s2 | rho | s2> )
+          * (< s1| rho | s2> / < s1| rho | s1>)
+         
+        importance_sampling_numerator(s3, Os1)  provides <s3 | rho | Os1>
+        importance_sampling_denominator(s3)     provides <s3 | rho | s3>
+        """
+        for copy_index in range(self.num_copy):
+            st = copy_index * samples.shape[1]
+            en = (copy_index+1) * samples.shape[1]
+            # numerator is []
+            numerator = nn_state.importance_sampling_numerator(swapped_samples_array[:, st:en], samples_array[:, st:en])
+            denominator = nn_state.importance_sampling_denominator(samples_array[:, st:en])
+            values = cplx.elementwise_division(numerator, denominator)
+            total_prod = cplx.elementwise_mult(total_prod, values)
+
+        value = cplx.real(total_prod)
+        return value
+    
+def obs_estimater(nn_state, pauli_dict: dict, num_samples: int, num_copies: int):
+    obs_num = GeneralPauliDistill(pauli_dict, num_copies)
+    obs_div = GeneralPauliDistill({}, num_copies)
+    num_stat = obs_num.statistics(nn_state, num_samples=num_samples)
+    div_stat = obs_div.statistics(nn_state, num_samples=num_samples)
+
+    from uncertainties import ufloat
+    num = ufloat(num_stat["mean"], num_stat["std_error"])
+    div = ufloat(div_stat["mean"], div_stat["std_error"])
+    val = num/div
+    result_dict = {"mean": val.n , "std_error": val.s, "num_samples": num_samples, "num_copies": num_copies}
+    return result_dict
