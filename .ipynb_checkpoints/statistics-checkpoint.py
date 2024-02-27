586a65093569e438bae23c8d0e936e9fd8d1324c
Fermi-D <ryomaekura@g.ecc.u-tokyo.ac.jp>
2024-02-27 21:07:40 +0900

    update

--- .ipynb_checkpoints/statistics-checkpoint.py
+++ .ipynb_checkpoints/statistics-checkpoint.py
@@ -4,17 +4,23 @@ import sampling_VD
 
 def bias(nn_state, ideal_state, n_copies, obs_mat):
     mat = utils.get_density_matrix(nn_state)
-    pow_mat = np.linalg.matrix_power(mat, n_copy)
+    pow_mat = np.linalg.matrix_power(mat, n_copies)
     vd_ev = np.trace(obs_mat@pow_mat) / np.trace(pow_mat)
     true_ev = np.trace(obs_mat @ ideal_state)
     
     return np.abs(true_ev - vd_ev)
 
-def variance(nn_state, pauli_dict, n_samples, n_copies):
-    obs_stat_dict = sampling_VD.obs_estimater(nn_state, pauli_dict, n_samples, n_copies)
-    return obs_stat_dict["std_error"]**2
+def variance(nn_state, pauli_dict, n_samples, n_copies, n_est):
+    ev_list = []
+    for _ in range(n_est):
+        ev_list.append(sampling_VD.obs_estimater(nn_state, pauli_dict, n_samples, n_copies)["mean"])
+    return np.var(ev_list)
 
-#def mse(nn_state, ideal_state, n_copy, obs_mat):
-    
-    
-    
+def rmse(nn_state, ideal_state, pauli_dict, n_copies, n_samples, n_est, obs_mat):
+    rmse_list = []
+    for _ in range(n_est):
+        sampling_vd_ev = sampling_VD.obs_estimater(nn_state, pauli_dict, n_samples, n_copies)["mean"]
+        true_ev = np.trace(obs_mat @ ideal_state)
+        rmse_list.append((true_ev - sampling_vd_ev)**2)
+        
+    return np.sqrt(np.mean(rmse_list))
